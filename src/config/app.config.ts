19ea84b8900243e8b4ea53078d732dc1f73e6282
liyuewen <1179374943@qq.com>
2022-11-24 10:36:28 +0800

    .

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -16,16 +16,16 @@ interface AppConfigType {
 export const AppConfig: AppConfigType = {
   mysql: {
     type: 'mysql',
-    host: '43.142.50.90',
+    host: '127.0.0.1',
     port: 3306,
-    username: 'lyw',
+    username: 'root',
     password: '123456',
     database: 'games',
     entities: ['dist/**/*.entity{ .ts,.js}'],
     synchronize: true,
   },
   redis: {
-    url: 'redis://43.142.50.90:6379',
+    url: 'redis://127.0.0.1:6379',
   },
   jwtConstants: {
     jwtKey: 'secretKey',
